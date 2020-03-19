7ab0524e8d3239eb744095bf580e8c7c184a5e86
Vigan Zeqiri <viganzeqirii@gmail.com>
2020-03-19 17:28:25 +0100

    Added dotenv library

--- controllers/image.js
+++ controllers/image.js
@@ -1,31 +1,33 @@
-const Clarifai = require('clarifai');
+const Clarifai = require("clarifai");
 
 //You must add your own API key here from Clarifai.
 const app = new Clarifai.App({
- apiKey: 'YOUR_API_KEY_HERE'
+	apiKey: process.env.CLARIFAI_KEY
 });
 
 const handleApiCall = (req, res) => {
-  app.models
-    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
-    .then(data => {
-      res.json(data);
-    })
-    .catch(err => res.status(400).json('unable to work with API'))
-}
+	app.models
+		.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+		.then(data => {
+			res.json(data);
+		})
+		.catch(err => res.status(400).json("unable to work with API"));
+};
 
 const handleImage = (req, res, db) => {
-  const { id } = req.body;
-  db('users').where('id', '=', id)
-  .increment('entries', 1)
-  .returning('entries')
-  .then(entries => {
-    res.json(entries[0]);
-  })
-  .catch(err => res.status(400).json('unable to get entries'))
-}
+	const { id } = req.body;
+
+	db("users")
+		.where("id", "=", id)
+		.increment("entries", 1)
+		.returning("entries")
+		.then(entries => {
+			res.json(entries[0]);
+		})
+		.catch(err => res.status(400).json("unable to get entries"));
+};
 
 module.exports = {
-  handleImage,
-  handleApiCall
-}
+	handleImage,
+	handleApiCall
+};
